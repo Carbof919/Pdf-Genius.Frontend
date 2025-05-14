69c954b12ff76ebfe1b36b078c803def733313f0
Carbof919 <carbof919@gmail.com>
2025-05-14 17:28:19 +0530

    Update Workspace.jsx

--- src/components/Workspace.jsx
+++ src/components/Workspace.jsx
@@ -1,1 +1,8 @@
-
+export default function Workspace() {
+  return (
+    <div className="flex-1 flex">
+      <div className="w-1/2 bg-gray-700 p-4">Document Viewer</div>
+      <div className="w-1/2 bg-gray-800 p-4">AI Chat</div>
+    </div>
+  );
+}
